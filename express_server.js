3edfe4c7e1960c4376c477656f5f3fdbfebbefb7
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-26 15:18:40 -0400

    Add POST /urls, adds shortURL and longURL to urlDatabase, redirects to url id page

--- express_server.js
+++ express_server.js
@@ -2,31 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
-const generateRandomString = () => {
-  const randomString = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
-  return randomString;
-};
-
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
-app.use(express.urlencoded({ extended: true }));
+const generateRandomString = () => {
+  const randomNum = Math.random().toString(20);
+  return randomNum.substring(2, 6);
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.get("/urls", (req, res) => {
@@ -34,20 +32,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = `http://${ req.body.longURL }`;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.url };
+  const siteID = req.params.id;
+  const templateVars = { id: siteID, longURL: urlDatabase[siteID] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send(`Ok ${generateRandomString()}`);
+app.get("/u/:id", (req, res) => {
+  const siteID = req.params.id;
+  const longURL = urlDatabase[siteID];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
