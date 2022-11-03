5c9130afb769a3883b4d5b3f3b9f2551d487f803
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-11-03 18:54:25 -0400

    Add helper functions from express_server

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+//return user if email matches a user email in database
 const getUserByEmail = (email, database) => {
   for (const data in database) {
     if (database[data].email === email) {
@@ -6,4 +7,21 @@ const getUserByEmail = (email, database) => {
   }
 };
 
-module.exports = { getUserByEmail };
+//returns a randomly generated 6 character string
+const generateRandomString = () => {
+  const randomNum = Math.random().toString(16);
+  return randomNum.substring(2, 8);
+};
+
+//returns an object of urls created by user
+const urlsForUser = (user ,database) => {
+  const userURLs = {};
+  for (const url in database) {
+    if (database[url].userID === user) {
+      userURLs[url] = database[url].longURL;
+    }
+  }
+  return userURLs;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
